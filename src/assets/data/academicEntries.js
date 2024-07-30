939bdd4a8ba6aaa761ebb06a075ee5c1cdc5ad90
dbarretol <57958754+dbarretol@users.noreply.github.com>
2024-07-29 22:27:32 -0500

    gh-pages deploy

--- src/assets/data/academicEntries.js
+++ src/assets/data/academicEntries.js
@@ -1,35 +1,35 @@
 // Puedes agregar más entradas en el futuro si es necesario
 const academicEntries = [
     {
-        logoSrc: "/images/EdLogos/ESAN.jpg",
+        logoSrc: "/dblaraBio/images/EdLogos/ESAN.jpg",
         logoAlt: "logo ESAN",
         program: "Maestría en administración de negocios (MBA)",
         institution: "ESAN Graduate School of Business",
         dates: "Julio/2023 - Agosto/2024"
     },
     {
-        logoSrc: "/images/EdLogos/UNI.jpg",
+        logoSrc: "/dblaraBio/images/EdLogos/UNI.jpg",
         logoAlt: "logo UNI",
         program: "Doctorado en ciencias con mención en energética",
         institution: "Universidad Nacional de Ingeniería",
         dates: "2016 - 2020"
     },
     {
-        logoSrc: "/images/EdLogos/UNI.jpg",
+        logoSrc: "/dblaraBio/images/EdLogos/UNI.jpg",
         logoAlt: "logo UNI",
         program: "Maestría en Gerencia e Ingeniería de Mantenimiento",
         institution: "Universidad Nacional de Ingeniería",
         dates: "2013 - 2015"
     },
     {
-        logoSrc: "/images/EdLogos/UNI.jpg",
+        logoSrc: "/dblaraBio/images/EdLogos/UNI.jpg",
         logoAlt: "logo UNI",
         program: "Pregrado en Ingeniería Mecánica-eléctrica",
         institution: "Universidad Nacional de Ingeniería",
         dates: "2016 - 2021"
     },
     {
-        logoSrc: "/images/EdLogos/UNI.jpg",
+        logoSrc: "/dblaraBio/images/EdLogos/UNI.jpg",
         logoAlt: "logo UNI",
         program: "Pregrado en Ingeniería Naval",
         institution: "Universidad Nacional de Ingeniería",
